a61a35d35fbfde52b2d2e3167bc8c3432ec68d99
Alfonso Presa <alfonso.presa@gmail.com>
2017-07-27 18:09:32 +0200

    fix(): duplicated failing line

--- src/main/java/com/bbva/arq/devops/ae/mirrorgate/collectors/jira/service/IssueTypeMapService.java
+++ src/main/java/com/bbva/arq/devops/ae/mirrorgate/collectors/jira/service/IssueTypeMapService.java
@@ -1,7 +1,5 @@
 package com.bbva.arq.devops.ae.mirrorgate.collectors.jira.service;
 
-import com.bbva.arq.devops.ae.mirrorgate.core.utils.IssueType;
-
 public interface IssueTypeMapService {
 
     String getIssueTypeFor(Long id);
