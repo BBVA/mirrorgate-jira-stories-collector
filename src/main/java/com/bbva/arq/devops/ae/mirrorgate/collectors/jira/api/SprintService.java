b46c8f0520646315bbfb755cdd59f0606a4f5f3f
kdaimiel <enrique.daimiel@bbva.com>
2019-03-04 13:44:43 +0100

    refactor(jira-collector): handle deleting issue error when the issue is already deleted

--- src/main/java/com/bbva/arq/devops/ae/mirrorgate/collectors/jira/api/SprintService.java
+++ src/main/java/com/bbva/arq/devops/ae/mirrorgate/collectors/jira/api/SprintService.java
@@ -19,21 +19,23 @@ package com.bbva.arq.devops.ae.mirrorgate.collectors.jira.api;
 import com.bbva.arq.devops.ae.mirrorgate.collectors.jira.config.Config;
 import com.bbva.arq.devops.ae.mirrorgate.collectors.jira.dto.IssueDTO;
 import com.bbva.arq.devops.ae.mirrorgate.collectors.jira.dto.SprintDTO;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Component
 public class SprintService {
@@ -70,7 +72,17 @@ public class SprintService {
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(mirrorGateUrl + MIRROR_GATE_DELETE_ISSUE_ENDPOINT).queryParams(params);
 
-        restTemplate.delete(builder.build().toUriString(), issueId);
+        try {
+            restTemplate.delete(builder.build().toUriString(), issueId);
+        } catch (final HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                LOGGER.info("Issue {} already deleted", issueId);
+            } else {
+                LOGGER.error("Error trying to delete issue {}", issueId, e);
+                throw e;
+            }
+        }
+
     }
 
     public List<SprintDTO> getSprintSamples() {
